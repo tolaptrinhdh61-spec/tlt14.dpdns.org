e0d3ed3d4fe5ecf0c01bf1948a381a9ff7db6fe1
ci-deploy@local <ci-deploy@local>
2025-12-23 15:17:21 +0700

    Auto deploy - 2025-12-23 08:17:21
    Modified: ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,17 +15,14 @@ module.exports = {
       max_restarts: 10,
       // ✅ PM2 nạp env từ file (mỗi lần start/restart)
       env_file: ENV_FILE,
-      env: {
-        APP_CWD: CWD,
-        NGINX_CONF_PATH: path.join(CWD, "nginx.conf"),
-        NGINX_PREFIX: path.join(CWD, "nginx"),
-        TZ: "Asia/Ho_Chi_Minh",
-        // ✅ QUAN TRỌNG: Phải truyền config vào đây
-        NGINX_CONF__BASE64__: process.env.NGINX_CONF__BASE64__ || "",
-        NGINX_CONF: process.env.NGINX_CONF || "",
-        TZ: "Asia/Ho_Chi_Minh",
-      },
     },
+    //   env: {
+    //     APP_CWD: CWD,
+    //     NGINX_CONF_PATH: path.join(CWD, "nginx.conf"),
+    //     NGINX_PREFIX: path.join(CWD, "nginx"),
+    //     TZ: "Asia/Ho_Chi_Minh",
+    //   },
+    // },
     {
       name: "cloudflared",
       script: "cloudflared",
@@ -36,9 +33,9 @@ module.exports = {
       max_restarts: 10,
       // ✅ PM2 nạp env từ file (mỗi lần start/restart)
       env_file: ENV_FILE,
-      env: {
-        TZ: "Asia/Ho_Chi_Minh",
-      },
+      // env: {
+      //   TZ: "Asia/Ho_Chi_Minh",
+      // },
     },
     {
       name: "envListener",
@@ -49,10 +46,10 @@ module.exports = {
       max_restarts: 10,
       // ✅ PM2 nạp env từ file (mỗi lần start/restart)
       env_file: ENV_FILE,
-      env: {
-        APP_CWD: CWD,
-        TZ: "Asia/Ho_Chi_Minh",
-      },
+      // env: {
+      //   APP_CWD: CWD,
+      //   TZ: "Asia/Ho_Chi_Minh",
+      // },
     },
   ],
 };
